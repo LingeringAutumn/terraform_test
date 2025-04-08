bce570e5303e42e6a38012249fbd3d4b77714c47
Orician <orician@nexsea.net>
2025-04-08 12:53:56 +0800

    更新.gitignore，添加对.tfvars文件的忽略；修正outputs.tf中Lambda函数调用命令的格式

--- outputs.tf
+++ outputs.tf
@@ -25,5 +25,5 @@ output "lambda_role_name" {
 
 output "lambda_invoke_command" {
   description = "调用Lambda函数的AWS CLI命令示例"
-  value       = "aws lambda invoke --function-name ${module.lambda_function.lambda_function_name} --payload '{}' response.json --region ${var.aws_region} --endpoint-url https://lambda.${var.aws_region}.amazonaws.com.cn"
+  value       = " aws lambda invoke --function-name ${module.lambda_function.lambda_function_name} --payload '{}' response.json --region ${var.aws_region} --endpoint-url https://lambda.${var.aws_region}.amazonaws.com.cn"
 }
